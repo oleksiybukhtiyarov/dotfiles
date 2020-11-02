072fae39e37d9ea58f7b2208f350d558a261a179
oleksiybukhtiyarov <50205720+oleksiybukhtiyarov@users.noreply.github.com>
2020-11-02 09:03:48 +0200

    Update get.go.sh

--- .local/get.go.sh
+++ .local/get.go.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-goversion=1.14.4
+goversion=1.15.3
 wget https://dl.google.com/go/go${goversion}.linux-amd64.tar.gz
 tar xvf go${goversion}.linux-amd64.tar.gz
 rm go${goversion}.linux-amd64.tar.gz
